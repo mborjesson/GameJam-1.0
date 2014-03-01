556e53cbd8d2e56716df4d0dd1ce667c9b6a947c
Erik <Erik@Erik-u35jc>
2014-03-02 00:28:10 +0100

    jump animation

--- src/gamejam10/character/Character.java
+++ src/gamejam10/character/Character.java
@@ -222,6 +222,7 @@ public abstract class Character extends LevelObject {
 
     HashMap<Facing, Animation> jumpAnimations;
     boolean showJumpingAnimation;
+    int currentIndex = 0;
     
     public void render(Graphics g, float offset_x, float offset_y){
     	
@@ -232,11 +233,12 @@ public abstract class Character extends LevelObject {
             
     		Animation anim = jumpAnimations.get(facing);
     		anim.draw(xp, yp);
-
+    		
     		// If we showed the last animation
-            if ( anim.getCurrentFrame() == anim.getImage(anim.getFrameCount()-1) ) {
+            if ( anim.getFrame() == anim.getFrameCount()-1 ) {
             	showJumpingAnimation = false;
             	anim.restart();
+            	currentIndex = 0;
             }
             
     	} else {
--- src/gamejam10/character/Player.java
+++ src/gamejam10/character/Player.java
@@ -36,7 +36,7 @@ public class Player extends Character {
        // setAnimation("data/images/characters/player/homeranim.png");
        setAnimation(/*"data/images/characters/player/homeranimsmall.png"*/"data/images/characters/player/mainanim.png", 20, 43, 8);
        
-       setJumpAnimation("data/images/characters/player/mainanimjump.png", 20, 43, 250, 5);
+       setJumpAnimation("data/images/characters/player/mainanimjump.png", 20, 43, 250, 3);
        
        //default bounding shape is a 32 by 32 box
 
